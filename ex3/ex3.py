e3241b2065311de9c601b7da6ff2e1381fadf0b1
Nimrod Mallis <nimrodmallis@gmail.com>
2024-12-09 17:22:54 +0200

    Implemented the most likely tag estimation, evaluation required

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -1,6 +1,52 @@
 import nltk
 from sklearn.model_selection import train_test_split
 
+class MostLikelyTag():
+    """
+    A class that assigns the most likely tag to a word, based on the training data,
+    using MLE for the estimation of the probabilities.
+    """
+    def __init__(self):
+        self.tag_count = {} # Count of each tag
+        self.word_tag_count = {} # Count of each word for each tag
+
+    def train(self, train_data):
+        """
+        Trains the model with the training data
+        """
+        # Each sentence is split to words and their respective tags (as tuples)
+        for sentence in train_data:
+            for word, tag in sentence:
+
+                # Counting the tags                
+                if tag not in self.tag_count:
+                    self.tag_count[tag] = 0
+                self.tag_count[tag] += 1
+
+                # Counting the words for each tag
+                if word not in self.word_tag_count:
+                    self.word_tag_count[word] = {}
+                if tag not in self.word_tag_count[word]:
+                    self.word_tag_count[word][tag] = 0
+                self.word_tag_count[word][tag] += 1
+
+    def predict(self, word):
+        # Handling unknown words (e.g. words that are not in the training data)
+        # by assigning the tag 'NN' as defined in the assignment
+        if word not in self.word_tag_count:
+            return 'NN'
+        
+        # Finding the tag with the highest probability for the word
+        max_prob = 0
+        max_tag = ''
+        for tag in self.word_tag_count[word]:
+            prob = self.word_tag_count[word][tag] / self.tag_count[tag]
+            if prob > max_prob:
+                max_prob = prob
+                max_tag = tag
+
+        return max_tag
+
 def get_dataset():
     """
     Retreives the tagged sentences of the 'news' category of the brown corpus.
@@ -18,7 +64,15 @@ def download_corpus():
 
 def main():
     download_corpus()
-    dataset = get_dataset()
+    # Task A - Getting the dataset
+    train_set, test_set = get_dataset()
+    
+    # Task B - Training a Most Likely Tag baseline
+    mlt = MostLikelyTag()
+    mlt.train(train_set)
+
+    ### TODO: Add evaluation here
+    
 
 if __name__ == '__main__':
     main()
